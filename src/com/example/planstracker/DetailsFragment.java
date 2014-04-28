e2a233ea165f24567e50f28a7d2e8f94f614e0aa
inv01 <gaiatoorion@gmail.com>
2014-04-28 17:40:25 +0200

    providing selected contact details and implementing phone calls from the app

--- src/com/example/planstracker/DetailsFragment.java
+++ src/com/example/planstracker/DetailsFragment.java
@@ -1,33 +1,28 @@
 package com.example.planstracker;
 
-import com.example.wokabstar.R;
-
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-//import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.Contacts;
-
 
 public class DetailsFragment extends Fragment {
   private String phoneNumber;
   private View view;
+  private EditText edt_email, edt_phone, edtContactName;
   
   public static DetailsFragment newInstance(int index) {
       DetailsFragment f = new DetailsFragment();
@@ -40,25 +35,32 @@ public class DetailsFragment extends Fragment {
   public int getShownIndex() {
       return getArguments().getInt("index", 0);
   }
-  
+
+ /* @SuppressWarnings("deprecation")
   public void setAdapter() {
-      //String[] in_words = mDbHelper.getWordsMatchingQuery(db, "");
-      //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, in_words);
-	  String[] FROM_COLUMNS = {
+      String[] FROM_COLUMNS = {
+          Contacts._ID,
           Build.VERSION.SDK_INT
                   >= Build.VERSION_CODES.HONEYCOMB ?
                   Contacts.DISPLAY_NAME_PRIMARY :
                   Contacts.DISPLAY_NAME
-	  };
-	  SimpleCursorAdapter mCursorAdapter = new SimpleCursorAdapter(
-              getActivity(),
-              R.layout.contact_list_item,
-              null,
-              FROM_COLUMNS, TO_IDS,
-              0);
-	  AutoCompleteTextView edtContactName = (AutoCompleteTextView) getActivity().findViewById(R.id.edt_name);
-	  edtContactName.setAdapter(mCursorAdapter);
-  }
+        };
+        CursorLoader c = new CursorLoader(getActivity(), 
+                ContactsContract.Contacts.CONTENT_URI, FROM_COLUMNS, 
+                null, null, null);
+        //mActivity, null, FROM_COLUMNS, phoneNumber, FROM_COLUMNS, phoneNumber);
+        Cursor cursor = c.loadInBackground();
+        SimpleCursorAdapter mCursorAdapter = new SimpleCursorAdapter(
+              getActivity().getBaseContext(),
+              0,
+              cursor,
+              FROM_COLUMNS, 
+              null);
+    AutoCompleteTextView edtContactName = (AutoCompleteTextView) view.findViewById(R.id.edt_name);
+    edtContactName.setAdapter(mCursorAdapter);
+  }*/
+
+  
   
   @Override
   public View onCreateView(LayoutInflater inflater,
@@ -66,8 +68,13 @@ public class DetailsFragment extends Fragment {
       if (container == null) {
           return null;
       }
+      
       view = inflater
         .inflate(R.layout.details_fragment, container, false);
+      edtContactName = (EditText) view.findViewById(R.id.edt_name);
+      edt_phone = (EditText) view.findViewById(R.id.edt_phone);
+      edt_email = (EditText) view.findViewById(R.id.edt_email);
+      //show date of event
       LinearLayout details = (LinearLayout) view.findViewById(R.id.date_layout);
       TextView text = new TextView(getActivity());
       int padding = (int)TypedValue.applyDimension(
@@ -76,15 +83,15 @@ public class DetailsFragment extends Fragment {
       text.setPadding(padding, padding, padding, padding);
       details.addView(text);
       text.setText("" + getShownIndex());
+      
+      //setAdapter();
       ImageView btnContact = (ImageView) view.findViewById(R.id.btnContact);
       btnContact.setOnClickListener(new OnClickListener(){
         @Override
         public void onClick(View v) {
           onContactClick(v);
         }
-        
       });
-       
       ImageView btnPhone = (ImageView) view.findViewById(R.id.btnPhone);
       btnPhone.setOnClickListener(new OnClickListener(){
         @Override
@@ -102,7 +109,6 @@ public class DetailsFragment extends Fragment {
         public void onClick(View v) {
           onEditClick(v);
         }
-        
       });
       ImageView btnSave = (ImageView) view.findViewById(R.id.btnSave);
       btnSave.setOnClickListener(new OnClickListener(){
@@ -110,7 +116,6 @@ public class DetailsFragment extends Fragment {
         public void onClick(View v) {
           onSaveClick(v);
         }
-        
       });
       ImageView btnDelete = (ImageView) view.findViewById(R.id.btnDelete);
       btnDelete.setOnClickListener(new OnClickListener(){
@@ -118,7 +123,6 @@ public class DetailsFragment extends Fragment {
         public void onClick(View v) {
           onDeleteClick(v);
         }
-        
       });
       ImageView btnRefresh = (ImageView) view.findViewById(R.id.btnRefreshLocation);
       btnRefresh.setOnClickListener(new OnClickListener(){
@@ -126,7 +130,6 @@ public class DetailsFragment extends Fragment {
         public void onClick(View v) {
           onRefreshLocationClick(v);
         }
-        
       });
       ImageView btnPicture = (ImageView) view.findViewById(R.id.btnPicture);
       btnPicture.setOnClickListener(new OnClickListener(){
@@ -134,18 +137,73 @@ public class DetailsFragment extends Fragment {
         public void onClick(View v) {
           onPictureClick(v);
         }
-        
       });
+      
+      TextView address = (TextView) view.findViewById(R.id.address);
+      address.setText("Austria, Klagenfurt; FloriangroegerStrasse 2");
       return view;
   }
   
   public void onContactClick(View view){
-    //
+      Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);  
+      startActivityForResult(intent, 1);
+  }
+  
+  @Override
+  public void onActivityResult(int reqCode, int resultCode, Intent data) {
+      super.onActivityResult(reqCode, resultCode, data);
+      if (resultCode == Activity.RESULT_OK) {
+          Uri contactData = data.getData();
+          ContentResolver cr = getActivity().getBaseContext().getContentResolver();
+          Cursor cur = cr.query(contactData, null, null, null, null);
+          if (cur.moveToFirst()) {
+              String id = cur.getString(
+                            cur.getColumnIndex(ContactsContract.Contacts._ID));
+              String name = cur.getString(
+                                  cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+              edtContactName.setText(name);
+              //getting first phone number
+              if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                  if (Integer.parseInt(cur.getString(
+                          cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                       Cursor pCur = cr.query(
+                   ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
+                   null, 
+                   ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?", 
+                   new String[]{id}, null);
+                     if (pCur.moveToFirst()) {
+                         String number = pCur.getString(pCur.
+                                 getColumnIndexOrThrow(ContactsContract.
+                                         CommonDataKinds.Phone.NUMBER));
+                         edt_phone.setText(number);
+                     }
+                     pCur.close();
+                  }
+              }
+              //getting first email address
+              Cursor emailCur = cr.query( 
+                      ContactsContract.CommonDataKinds.Email.CONTENT_URI, 
+                      null,
+                      ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", 
+                      new String[]{id}, null); 
+                    if (emailCur.moveToFirst()) { 
+                        // This would allow you get several email addresses
+                              // if the email addresses were stored in an array
+                        String email = emailCur.getString(
+                                        emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                        edt_email.setText(email);
+                        //String emailType = emailCur.getString(
+                        //                emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE)); 
+                    } 
+                    emailCur.close();
+              // END getting first email address
+             }
+      }  
   }
   
   public void onEditClick(View view){
-      
-    }
+    
+  }
   public void onSaveClick(View view){
     
   }
